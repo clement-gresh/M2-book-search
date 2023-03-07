17b446ee0fe5cb63f97b3ee913cea9ab4af35012
LIU Yajie <yajie.liu@u-psud.fr>
2023-03-07 12:21:49 +0100

    Initial commit

---
+++ src/main/java/com/searchgutenberg/booksearchengine/BooksearchengineApplication.java
@@ -0,0 +1,13 @@
+package com.searchgutenberg.booksearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksearchengineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksearchengineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/searchgutenberg/booksearchengine/BooksearchengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchgutenberg.booksearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksearchengineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
